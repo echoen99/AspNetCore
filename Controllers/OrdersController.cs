9c43caadd38f597fffdeabc434f1d5fc79bac006
Yosi Angel <Yosi.Angel@888holdings.com>
2019-11-07 20:28:52 +0200

    Add project files.

---
+++ Controllers/OrdersController.cs
@@ -0,0 +1,78 @@
+﻿using DutchTreat.Data;
+using DutchTreat.Data.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DutchTreat.Controllers
+{
+  [Route("api/[Controller]")]
+  public class OrdersController : ControllerBase
+  {
+    private readonly IDutchRepository _repository;
+    private readonly ILogger<OrdersController> _logger;
+
+    public OrdersController(IDutchRepository repository, ILogger<OrdersController> logger)
+    {
+      _repository = repository;
+      _logger = logger;
+    }
+
+    public  IActionResult Get()
+    {
+      try
+      {
+        return Ok(_repository.GetAllOrders());
+      }
+      catch (Exception ex)
+      {
+        _logger.LogError($"Failed to get orders: {ex}");
+        return BadRequest("Failed to get orders");
+      }
+    }
+
+    [HttpGet("{id:int}")]
+    public IActionResult Get(int id)
+    {
+      try
+      {
+
+        var order = _repository.GetOrderById(id);
+        if (order != null) return Ok(order);
+        else return NotFound();
+      }
+      catch (Exception ex)
+      {
+        _logger.LogError($"Failed to get orders: {ex}");
+        return BadRequest("Failed to get orders");
+      }
+
+    }
+
+    [HttpPost]
+    public  IActionResult Post([FromBody]Order model)
+    {
+      // Add it to the DB
+      try
+      {
+        _repository.AddEntity(model);
+        if(_repository.SaveAll())
+        {
+          return Created($"/api/orders/{model.Id}", model);
+        }
+
+      }
+      catch (Exception ex)
+      {
+        _logger.LogError($"Failed to save a new order: {ex}");
+      }
+
+      return BadRequest("Failed to save new order");
+    }
+
+  }
+}
---
+++ Startup.cs
@@ -0,0 +1,78 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DutchTreat.Data;
+using DutchTreat.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace DutchTreat
+{
+  public class Startup
+  {
+    private readonly IConfiguration _config;
+
+    public Startup(IConfiguration config)
+    {
+      _config = config;
+    }
+    // This method gets called by the runtime. Use this method to add services to the container.
+    // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.AddDbContext<DutchContext>(cfg =>
+      {
+        cfg.UseSqlServer(_config.GetConnectionString("DutchConnectionString"));
+      });
+      services.AddTransient<IMailService, NullMailService>();
+
+      services.AddTransient<DutchSeeder>();
+
+      services.AddScoped<IDutchRepository, DutchRepository>();
+
+      services.AddMvc()
+          .AddNewtonsoftJson(opt => opt.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore);
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+      }
+      else
+      {
+        // Add Error Page
+        app.UseExceptionHandler("/Error");
+      }
+
+      app.UseStaticFiles();
+      app.UseNodeModules();
+
+      app.UseRouting();
+
+      app.UseEndpoints(cfg =>
+      {
+        cfg.MapControllerRoute("Fallback",
+                  "{controller}/{action}/{id?}",
+                  new { controller = "App", action = "Index" });
+
+        cfg.MapRazorPages();
+      });
+
+      //app.Run(async context =>
+      //    {
+      //        await context.Response.WriteAsync("<html><body><h1>Hello World!</h1></body></html>");
+      //    });
+    }
+  }
+}
